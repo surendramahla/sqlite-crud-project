548b9386259e4c8c2de3e082ef91053f8df93d6e
SurendraMahla <surendramahla>
2026-02-17 21:30:05 +0530

    SQLite CRUD project

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sqlite3
 
+
 conn = sqlite3.connect("abc.db")
+
 cursor = conn.cursor()
 
 cursor.execute("""
@@ -8,68 +10,88 @@ CREATE TABLE IF NOT EXISTS students(
 id INTEGER PRIMARY KEY AUTOINCREMENT,
 name TEXT NOT NULL,
 age INTEGER,
-grade TEXT)
-""")
+grade TEXT)                              
+               """)
 
-# Insert one valid record
-cursor.execute(
-    "INSERT INTO students (name, age, grade) VALUES (?, ?, ?)",
-    ("BOB", 22, "B++")
-)
-conn.commit()   # Important!
+#name = "Tom"
+# web app form or sink
+# sqllit3 dosn't allow multiple sql statements to run. that's why it's safe
+"""name = "Tom); DROP TABLE students; --"
+cursor.execute(f"INSERT INTO students (name) VALUES  ('{name}')")
 
-print("Before rollback test:")
-cursor.execute("SELECT * FROM students")
-rows = cursor.fetchall()
-for row in rows:
-    print(row)
+conn.commit()"""
 
-# Now test rollback
+# parameterised query that prevent sql injectio attack
+"""cursor.execute("insert into students (name, age, grade) values (?, ?, ?)",
+                ("BOB", 22, "B++"))
+
+conn.commit()"""
+
+"""cursor.execute("UPDATE students SET grade = ? WHERE id = ?", ("A++",2))
+conn.commit()"""
+
+"""student_l = [
+    ("abc", 55, "B"),
+    ("BotTheDog", 33, "A"),
+    ("abcdfas", 26, "C")
+]
+
+cursor.executemany("INSERT INTO students (name, age, grade) VALUES (?, ?, ?)", 
+                   student_l)
+conn.commit()"""
+
+## DELETE DATA
+
+"""cursor.execute("DELETE FROM students WHERE id = ?", (4,))
+conn.commit()"""
+
+## ORDER by
+#cursor.execute("SELECT * FROM students ORDER BY age")
+#cursor.execute("SELECT * FROM students ORDER BY age DESC")
+#cursor.execute("SELECT * FROM students ORDER BY age DESC LIMIT 2")
+#####
+
+
+# Find data by patern like find all the name which has "om"
+
+#cursor.execute("SELECT * FROM students WHERE name LIKE ?", ("%om%",))
+
+#
+#rows = cursor.fetchall()
+
+# Transection and roleback
+# Prevent the database curption
 try:
-    cursor.execute(
-        "INSERT INTO students (name, age, grade) VALUES (?, ?, ?)",
-        ("Alice", 20, "A")
-    )
-    raise Exception("Simulated error")
+    cursor.execute('INSERT INTO students (name, age, grade) VALUE (?, ?, ?)', 
+                   ('raju', 55, 'B+'))
+    raise Exception("error")
 except:
     conn.rollback()
-    print("\nRollback cancelled the error query")
-
-print("\nAfter rollback test:")
-cursor.execute("SELECT * FROM students")
-rows = cursor.fetchall()
-for row in rows:
-    print(row)
+    print("roleback cancle the error query")
 
 
 
-def add_details():
-    conn = sqlite3.connect("abc.db")
-    cursor = conn.cursor()
-
-    # 5-7 students sample data
-    students = [
-        {"name": "Rahul", "age": 21, "grade": "A"},
-        {"name": "Amit", "age": 22, "grade": "B+"},
-        {"name": "Sneha", "age": 20, "grade": "A+"},
-        {"name": "Priya", "age": 23, "grade": "B"},
-        {"name": "Rohan", "age": 21, "grade": "C"},
-        {"name": "Anjali", "age": 22, "grade": "A"},
-        {"name": "Karan", "age": 24, "grade": "B"}
-    ]
-
-    cursor.executemany(
-        "INSERT INTO students (name, age, grade) VALUES (:name, :age, :grade)",
-        students
-    )
-
+def add_details(name,age,grade):
+    cursor.execute("INSERT INTO students (name, age, grade) VALUES (?, ?, ?)", (name, age, grade))
     conn.commit()
-    conn.close()
+    print("DATA inserted")
 
-    print("7 Students added successfully ✅")
+def data_del(id):
+    cursor.execute("DELETE FROM students WHERE id = ?", (id,))
+    conn.commit()
+    print(f"The row which has {id} deleted")
+def show_tab():
+    cursor.execute("SELECT * FROM students")
+    rows = cursor.fetchall()
+    for row in rows:       
+        print(row)
 
+#add_details("BOBTHEDOG", 11, "A")
 
-add_details()
+"""cursor.execute("SELECT * FROM students")
+rows = cursor.fetchall()
 
-conn.close()
+for row in rows:
+    print(row)"""
 
+show_tab()
